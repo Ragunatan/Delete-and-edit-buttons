065a3c4c4cfb89d3afe7d58b1f4d01fe1fd3ffc3
Ragunatan <ddonnatan@gmail.com>
2023-07-24 21:33:07 +0530

    made items bold and 3rd item bg green

--- demo.js
+++ demo.js
@@ -1,9 +1,13 @@
 const header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000'
-var items = document.getElementsByClassName('list-group-item')
+
 
 var titles = document.getElementsByClassName('titles')
 titles[0].style.fontWeight = 'bold'
 titles[0].style.color = 'green'
 
-
+var items = document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor = 'green'
+for(i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold'
+}
