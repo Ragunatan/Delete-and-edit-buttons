a295b0d262473d3802e2476dc180e0759ed8dcb1
Ragunatan <ddonnatan@gmail.com>
2023-07-24 21:26:41 +0530

    made font color green

--- demo.js
+++ demo.js
@@ -1,1 +1,9 @@
-const third = 
+const header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000'
+var items = document.getElementsByClassName('list-group-item')
+
+var titles = document.getElementsByClassName('titles')
+titles[0].style.fontWeight = 'bold'
+titles[0].style.color = 'green'
+
+
