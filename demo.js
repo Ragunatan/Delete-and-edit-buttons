ad526afc2ae3a3a3bcf4e66366c0ae8b36178d6b
Ragunatan <ddonnatan@gmail.com>
2023-07-25 14:11:34 +0530

    added green background for odd items

--- demo.js
+++ demo.js
@@ -1,13 +1,16 @@
 const header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000'
 
+// var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+// seconditem.style.backgroundColor = 'green'
 
-var titles = document.getElementsByClassName('titles')
-titles[0].style.fontWeight = 'bold'
-titles[0].style.color = 'green'
+// var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+// thirditem.style.display = 'none'
 
-var items = document.getElementsByClassName('list-group-item')
-items[2].style.backgroundColor = 'green'
+var items = document.querySelectorAll('.list-group-item')
+items[1].style.color = 'blue'
 for(i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold'
+    if(i%2 ===1){
+        items[i].style.backgroundColor = 'green'
+    }
 }
