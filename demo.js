61e7b635c798d33186d3f506b48f8b3d11f1bdbc
Ragunatan <ddonnatan@gmail.com>
2023-07-25 22:51:42 +0530

    parent node append

---
+++ demo.js
@@ -0,0 +1,17 @@
+var de = document.querySelector('#items')
+
+
+var newdiv = document.createElement('div')
+newdiv.className = 'head';
+newdiv.id = '125'
+newdiv.setAttribute('title','hello world')
+var newtext = document.createTextNode('Hello-World')
+newdiv.appendChild(newtext)
+// console.log(newdiv)
+
+var container = document.querySelector('header .container')
+var h1 = document.querySelector('header h1')
+container.insertBefore(newdiv , h1)
+
+var child1 = document.getElementById('items')
+child1.prepend(newdiv)
